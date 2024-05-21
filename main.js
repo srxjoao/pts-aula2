d081f696705ebcab4b9b0be0e66191b562668812
srxjoao <joao.barbosa6@estudante.ifms.edu.br>
2024-05-21 07:54:02 -0400

    atividades

---
+++ main.js
@@ -0,0 +1,10 @@
+class Produto{
+    constructor(nome,preco,quantidade){
+        this.nome = Iphone;
+        this.preco = 5000;
+        this.quantidade = 15;
+    }
+    valorTotal(){
+        console.log('este e o preço${this.nome}e tenho a ${quantidade} disponivel eo valor é ${this.preco}');
+    }
+}
